58fcc6e8ff2e858d7a05be9a997b704bc7c3725a
David Trinh <dtrinh@dtrinh.local>
2017-01-10 19:53:16 -0800

    updated with basic skeleton

--- node_helper.js
+++ node_helper.js
@@ -3,17 +3,12 @@ var moment = require('moment');
 var _ = require('lodash');
 var NodeHelper = require('node_helper');
 
-module.exports = NodeHelper.create({});
-
 function getNBASchedule() {
   return axios.get('http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2016/league/00_full_schedule.json')
     .then(function(data){
       return data.data.lscd;
     });
 }
-
-var now = moment().format('YYYY MM DD');
-var currentMonth = moment().format('MMMM');
 
 function filterByMonth(month, data) {
   for (var i = 0; i <= data.length; i++) {
@@ -24,15 +19,16 @@ function filterByMonth(month, data) {
   }
 }
 
-var getData = function() {
+var getDataFromNBA = function() {
   return getNBASchedule().then(function(payload) {
+      var currentMonth = moment().format('MMMM');
       return filterByMonth(currentMonth, payload);
   }).then(function(result) {
       return result
   });
 }
 
-var getMonthlySchedule = function(data, month){
+var getMonthlySchedule = function(data, month, teamId){
 
   var dataSet = [];
 
@@ -40,7 +36,7 @@ var getMonthlySchedule = function(data, month){
      if (data[k].mscd.mon === month) {
        var currentMonthlySchedule = data[k].mscd.g;
        for (var i = 0; i < currentMonthlySchedule.length; i++) {
-          if (currentMonthlySchedule[i].v.tid === 1610612744 || currentMonthlySchedule[i].h.tid === 1610612744) {
+          if (currentMonthlySchedule[i].v.tid === teamId || currentMonthlySchedule[i].h.tid === teamId) {
            dataSet.push(currentMonthlySchedule[i]);
           }
        }
@@ -50,17 +46,123 @@ var getMonthlySchedule = function(data, month){
   return dataSet;
 };
 
-var getSchedule = function(month) {
-  return getData().then(function(payload) {
-    return getMonthlySchedule(payload, month)
+var getTeamSchedule = function(teamId) {
+  return getDataFromNBA().then(function(payload) {
+    return getMonthlySchedule(payload, month, teamId)
   })
 };
 
-var monthlyScheduleResults = function() {
-  return getSchedule(currentMonth).then(function(r) {
+var monthlyScheduleResults = function(teamId) {
+  return getTeamSchedule(teamId).then(function(r) {
     console.log("result of monthlyResults", r, r.length)
     return r
   });
 };
 
-monthlyScheduleResults();
+monthlyScheduleResults(1610612744);
+
+module.exports = NodeHelper.create({
+
+  url: "http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2016/league/00_full_schedule.json",
+  scores: [],
+  details: {},
+  nextMatch: null,
+  live: {
+    state: false,
+    matches: []
+  },
+
+  start: function() {
+    console.log("Starting Module: " + this.name);
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "CONFIG") {
+      this.config = payload.config;
+      //this.teams = payload.teams;
+      this.getData();
+      setInterval(() => {
+        this.getData();
+      }, this.config.reloadInterval);
+      setInterval(() => {
+        this.fetchOnLiveState();
+      }, 60*1000);
+    }
+  },
+
+  fetchOnLiveState: function() {
+    if (this.live.state === true) {
+      this.getData();
+    }
+  },
+
+  getData: function() {
+    var axios = require('axios');
+    var _ = require('lodash');
+
+    function checkResponseStatus(res) {
+      if (res.status !== 200) {
+        throw new Error(res);
+      } else {
+        return res;
+      }
+    }
+
+    function massageResponseData(data) {
+      return data.data.lscd;
+    }
+
+    function getCurrentMonth() {
+
+    }
+
+    function getTeamsCurrentRecord() {
+
+    }
+
+    function getCurrentSchedule(data) {
+      var schedule = []
+      for (var mscdObj = 0; mscdObj < data.length; mscdObj++) {
+          //schedule.push(data[mscdObj].mscd.g)
+        var gameSchedule = data[mscdObj].mscd.g;
+        for (var gameObj = 0; gameObj < gameSchedule.length; gameObj++) {
+          schedule.push(gameSchedule[gameObj])
+        }
+      }
+      return schedule;
+    }
+
+    function filterForTeamSchedule(schedule, teamId) {
+      var teamSchedule = [];
+
+      _.filter(schedule, function(fSch) {
+        teamSchedule.push(fSch.v.tid === teamId || fSch.h.tid === teamId)
+      })
+
+      return teamSchedule;
+    }
+
+    function getTeamSchedule(result) {
+      console.log(result)
+      return result;
+    }
+
+
+    var getData = function(url, teamId) {
+      return axios.get(url)
+      .then(checkResponseStatus)
+      .then(massageResponseData)
+      .then(getCurrentSchedule)
+      .then(getTeamSchedule)
+      .catch(function(error) {
+        if (error.response) {
+          console.log(error.response.data)
+          console.log(error.response.status)
+        } else {
+          console.log("Error with getData", error.message)
+        }
+      });
+    }
+
+    getData('http://data.nba.com/data/10s/v2015/json/mobile_teams/nba/2016/league/00_full_schedule.json', 1610612744);
+});
